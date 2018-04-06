4d63b01b797ec8e01d7115a698c1f77479da1c2e
Mhiho <m.pelka7@gmail.com>
2018-04-06 11:48:03 +0200

    stbaseOfPaginationDone

--- src/containers/Photos/Photos.js
+++ src/containers/Photos/Photos.js
@@ -7,23 +7,27 @@ class Photos extends Component{
   constructor(props){
     super(props)
   }
-  state = {
-    photos: []
+  state= {
+    photos: [],
+    x: 0,
+    y: 12,
+    initialPage: 0
   }
-
   componentDidMount() {
     axios.get( 'https://jsonplaceholder.typicode.com/photos' )
       .then( response => {
         console.log(response);
-        const photos = response.data.slice(0,12);
+        const all = response.data;
+        const photos = response.data.slice(this.state.x+(12*this.state.initialPage), this.state.y+(12*this.state.initialPage));
         const updatedPhotos = photos.map(photo => {
           return {
             ...photo
           }
-        })
+        });
+
         this.setState({photos: updatedPhotos})
       });
-  };
+  }
 
   render() {
     const renderPhotos = this.state.photos.map(photo =>{
